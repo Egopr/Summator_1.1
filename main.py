b1a56edfdcfbb947a58053133bffcd1a31779259
EgorKA <Egor_mail_no@mail.ru>
2024-05-06 08:28:20 +0300

    New

--- main.py
+++ main.py
@@ -3,3 +3,12 @@
 Содержит основные окна для работы.
 '''
 
+from PyQt5 import QtWidgets, uic
+import sys
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+windows = uic.loadUi("summator_0.1.ui")
+windows.show()
+sys.exit(app.exec())
